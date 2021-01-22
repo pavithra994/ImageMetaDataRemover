9314d4bb779ad0e15f94f0ba5813c18aa72184a0
Pavithra Weerapperuma <36977042+pavithra994@users.noreply.github.com>
2021-01-23 00:13:59 +0530

    old file deleting

--- run.py
+++ run.py
@@ -1,23 +1,62 @@
 from PIL import Image
-
-
 import os
-files = [f for f in os.listdir('.') if os.path.isfile(f) and not f.endswith(".py")]
+from time import sleep
+
+
+FOLDER_NAME = "Images"
+
+def delete_file(file_list:[],initial_print_lines=""):
+    for i,file in enumerate(file_list):
+        y = i+1
+        x = len(file_list) - y
+        p = (y/len(file_list))*100 if y != 0 else 0
+        os.system('cls')
+        print(initial_print_lines)
+        print("Deleting old files... {}{} : {:.2f}%".format("|"*y,"="*x,p))
+        os.remove(file)
+        sleep(1)
+
+    os.system('cls')
+    print(initial_print_lines)
+    print("Total {} files are deleted".format(len(file_list)))
+
+
+
+files = [f for f in os.listdir('.') if os.path.isfile(f) and not f.lower().endswith(".py")]
 files = sorted(files)
-print(files)
+
+if not os.path.exists(FOLDER_NAME):
+    os.makedirs(FOLDER_NAME)
+
+completed_files = []
+error_files = []
+
 for i, f in enumerate(files):
-    print(f,type(f))
+
+    print("Meta data cleaning... : {}/{}".format(i+1,len(files)))
     try:
         formt = f.split(".")[-1]
         image = Image.open(f)
-
-        # next 3 lines strip exif
         data = list(image.getdata())
         image_without_exif = Image.new(image.mode, image.size)
         image_without_exif.putdata(data)
-
-        image_without_exif.save("New folder/{}.{}".format(str(i).zfill(4),formt))
+        image_without_exif.save("{}/{}.{}".format(FOLDER_NAME,str(i).zfill(4),formt))
+        completed_files.append(f)
     except Exception as e:
-        print(e)
+        error_files.append({"file":f, "error":e})
 
+    os.system('cls')
 
+print("Completed ----- {}/{}".format(len(completed_files),len(files)))
+
+delete_file(completed_files,"Completed ----- {}/{}".format(len(completed_files),len(files)))
+
+if len(completed_files) == len(files):
+    print("All operations are done....")
+    sleep(2)
+else:
+    print("Errors occur in following files : ")
+    for ef in error_files:
+        print("{} : {} ".format(ef["file"],ef["error"]))
+
+    x = input("press any key to close...")
